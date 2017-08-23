5ecb03506a919699a7fa2a1363af3d3291915e6d
Your Name <mayank@emial.com>
2017-08-23 00:50:25 -0700

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
-Placeholder
+FROM fnndsc/ubuntu-python3
+
+RUN mkdir -p /usr/app && sed -i "s/^exit 101$/exit 0/" /usr/sbin/policy-rc.d
+COPY ./* /usr/app/
+WORKDIR /usr/app
+RUN pip3 install --no-cache-dir -r requirements.txt 
+RUN python3 get-agent.py && chmod +x install_agent.bin
+CMD ./install_agent.bin && tail -f /dev/null
--- get-agent.py
+++ get-agent.py
@@ -9,41 +9,54 @@ class st_agent:
     def __init__(self):
         
         # External Parameters
-        self.apikey = os.environ['APIKEY']
-        self.apisecret = os.environ['APISECRET']
-        self.orgname = os.environ['CUSTOMERID']
+        with open('secrets.json') as json_data:
+            secrets = json.load(json_data)
+            self.apikey = secrets['apikey']
+            self.apisecret = secrets['apisecret']
+            self.orgname = secrets['customerid']
         
         # Place holders
         self.token = ''
+        self.companyid = ''
+        self.role = ''
         
-        # Statiic default parameters
-        self.output = '/tmp/smart-tools-agent.bin'
+        # Static default parameters
+        self.output = os.path.dirname(os.path.realpath(__file__)) + '/install_agent.bin'
+        self.debug = False
 
     def get_oAuthToken(self):
         
         # Get CompanyId
-        url = make_url("https://smart.cloud.com/v0/companies?", { 'client_id' : self.apikey} )
+        url = make_url("https://smart.cloud.com/v0/companies?", { 'client_id' : self.apikey } )
         response = json.loads(requests.request("GET", url).text)
+        if self.debug:
+            debug(response)
         data = (item for item in response if item["name"] == self.orgname)
         for item in data:
-            companyid = str(item["companyId"])
+            self.companyid = str(item["companyId"])
             break
         
         # Get API client's role based on apikey
-        url = make_url("https://smart.cloud.com/v0/roles?", { 'client_id' : self.apikey, 'company_id' : companyid})
-        role = json.loads(requests.request("GET", url).text)[0]
+        url = make_url("https://smart.cloud.com/v0/roles?", { 'client_id' : self.apikey, 'company_id' : self.companyid })
+        self.role = json.loads(requests.request("GET", url).text)[0]
+        if self.debug:
+            debug(self.role)
         
         # Get OAuth Token
-        url = make_url("https://smart.cloud.com/v0/oauth/token?", { 'grant_type' : 'client_credentials', 'scope' : role + "," + companyid})
+        url = make_url("https://smart.cloud.com/v0/oauth/token?", { 'grant_type' : 'client_credentials', 'scope' : self.role + "," + self.companyid})
         apikeysecret = self.apikey + ":" + self.apisecret
         apikeysecret = base64.b64encode(apikeysecret.encode())
         headers = {'Authorization': 'Basic ' + apikeysecret.decode('UTF-8') }
         self.token = json.loads(requests.request("GET", url, headers=headers).text)['access_token']
+        if self.debug:
+            debug(self.token)
 
     def get_agent(self):
         url = make_url("https://smart.cloud.com/v0/download/info?", {'access_token' : self.token} )
         response = json.loads(requests.request("GET", url).text)
-        data = (item for item in response['data'] if item['arch'] == 'deb32')
+        if self.debug:
+            debug(response)
+        data = (item for item in response['data'] if item['arch'] == 'deb64')
         for item in data:
             link = item["link"]
             break
